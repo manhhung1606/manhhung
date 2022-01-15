3c9256b19f8c7e378ac5e73b770ffc070ae8b71b
QuynhHoa888 <88197704+QuynhHoa888@users.noreply.github.com>
2022-01-15 21:10:30 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Welcome to www.hungdeptrai.com!!!',
+    introDesc: ` Website dành cho người trên 69 tuổi!!! `,
+    btnIntro: ' Nếu đủ rồi thì click here !',
+    title: ' Đằng ấy có thấy tớ xinhh gái hơm???😘 ',
+    desc: 'Nếu mà nói dối là cậu sẽ ế suốt đời🤭🤭🤭 ',
+    btnYes: 'Đẹp zaiiii  <33',
+    btnNo: 'Em ko dám nói ạ😭😭😭',
+    question: ' Nhóc mệt rồi hả🤣🤣🤣 ',
+    btnReply: 'Send to Admin',
+    reply: 'Đm, em thề ...A Hùng đẹp zai vl...',
+    mess: 'Ahihi,Thật thà vậy có phải đáng iu ko 😁😁😁',
+    messDesc: ' Nói vậy chắc là bạn cũng nóng lòng xem mình đẹp zai ntn lắm rồi đúng k ',
+    btnAccept: ' Vậy thì... Lettsssss goo!!!',
+    messLink: 'https://www.facebook.com/waybackhome88'
+}
