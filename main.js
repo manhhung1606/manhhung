bddd1ef0516787b4870fe4a9d7a353eabf0d0584
QuynhHoa888 <88197704+QuynhHoa888@users.noreply.github.com>
2022-03-07 00:20:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function firstQuestion(){
 
  // switch button position
  function switchButton() {
-    var audio = new Audio('https://hungdeptrai.com/nicolas_cage_laughing_7943556063031366853 (mp3cut.net).mp3');
+    var audio = new Audio('https://hungdeptrai.com/X2Download.com - Savage Love meme __ Gacha Club (Flipaclip) (128 kbps).mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
@@ -48,7 +48,7 @@ function firstQuestion(){
 }
 // move random button póition
 function moveButton() {
-    var audio = new Audio('https://hungdeptrai.com/nicolas_cage_laughing_7943556063031366853 (mp3cut.net).mp3');
+    var audio = new Audio('https://hungdeptrai.com/X2Download.com - Savage Love meme __ Gacha Club (Flipaclip) (128 kbps).mp3');
     audio.play();
     var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9 ;
     var y = Math.random() * ($(window).height() - $('#no').height()) * 0.3;
