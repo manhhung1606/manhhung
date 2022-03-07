444f73aa4fa7efebdb7b207401402e2fa844121c
QuynhHoa888 <88197704+QuynhHoa888@users.noreply.github.com>
2022-03-07 20:46:10 +0700

    Update hunglanhlung.js

--- hunglanhlung.js
+++ hunglanhlung.js
@@ -121,7 +121,7 @@ window.addEventListener("contextmenu", function(evt) {
 
 var css = document.createElement("style");
 css.type = "text/css";
-css.innerHTML = "* { color: #white;font-family: \"VT323\" !important; }";
+css.innerHTML = "* { color: #white;font-family: \"VT323\" ; }";
 document.body.appendChild(css);
 
 
