5b159e10641128279db61af32abcd4e67b3e76e7
QuynhHoa888 <88197704+QuynhHoa888@users.noreply.github.com>
2022-05-15 16:14:16 +0700

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,5 +1,5 @@
 
-   audio = new Audio("https://hungdeptrai.com/X2Download.com - TheFatRat & Anjulie - Close To The Sun x Origin (128 kbps).mp3");
+   audio = new Audio("https://hungdeptrai.com/tinh_yeu_mau_hong_lofi_ver_ho_van_quy_x_xam_x_freak_d_-3194897992726004723.mp3");
 
 document.onclick = function() {
   audio.play();
