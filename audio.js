cd009c28a6655dd9ba18130378b506c649e2fee7
QuynhHoa888 <88197704+QuynhHoa888@users.noreply.github.com>
2022-05-25 07:01:19 +0700

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,5 +1,5 @@
 
-   audio = new Audio("https://hungdeptrai.com/past_lives_born_sapientdream_603077226658670431 (1).mp3");
+   audio = new Audio("https://hungdeptrai.com/Alan_Walker_-_Sunday_Music_Video_(getmp3.pro).mp3");
 
 document.onclick = function() {
   audio.play();
