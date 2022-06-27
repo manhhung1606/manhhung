f529379fe43e11351c2ebbf01a1e31928d2e0dad
QuynhHoa888 <88197704+QuynhHoa888@users.noreply.github.com>
2022-06-27 07:02:42 +0700

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,5 +1,5 @@
 
-   audio = new Audio("https://hungdeptrai.com/Mt_Bc_Yu_Vn_Dm_au_Lyrics_V_(getmp3.pro).mp3");
+   audio = new Audio("https://hungdeptrai.com/Past_Lives_Violin_Cover_Bn_F_(getmp3.pro).mp3");
 
 document.onclick = function() {
   audio.play();
