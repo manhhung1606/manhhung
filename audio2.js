02c4b415678ea161c443190453e50acee828fc7d
manhhung1606 <88197704+manhhung1606@users.noreply.github.com>
2023-11-08 10:52:25 +0700

    Update audio2.js
    Signed-off-by: manhhung1606 <88197704+manhhung1606@users.noreply.github.com>

--- audio2.js
+++ audio2.js
@@ -9,6 +9,7 @@ const ManhHung_List = [
 "https://manhhung1606.github.io/manhhung/Mt_Bc_Yu_Vn_Dm_au_Lyrics_V_(getmp3.pro).mp3",
 "https://manhhung1606.github.io/manhhung/X2Download.app - █▬█ █ ▀█▀ Bài Học Trong Cuộc Sống - Tiay (Rap Việt 2012) (320 kbps).mp3",
 "https://manhhung1606.github.io/manhhung/AI_L_NGI_THNG_EM_-_QUN_AP_L_(getmp3.pro).mp3",
+"https://manhhung1606.github.io/manhhung/Please Tell Me Why - Bảo Thy (ft. Vương Khang)  Lyrics Video.mp3",
   ];
   //Random music
   let index = Math.floor(Math.random() * ManhHung_List.length);
