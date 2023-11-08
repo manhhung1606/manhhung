2ff7b46ecf53dcb1c506da9afb6c6b2ba0412226
manhhung1606 <88197704+manhhung1606@users.noreply.github.com>
2023-11-08 09:24:15 +0700

    Update audio2.js
    Signed-off-by: manhhung1606 <88197704+manhhung1606@users.noreply.github.com>

--- audio2.js
+++ audio2.js
@@ -7,6 +7,7 @@ const ManhHung_List = [
 "https://manhhung1606.github.io/manhhung/vietsub_ai_phi_hau_huyen_-224110807638924782.mp3",
 "https://manhhung1606.github.io/manhhung/Ong Ba Gia Tao Lo Het - Binh Gold_ Lil S.mp3",
 "https://manhhung1606.github.io/manhhung/Mt_Bc_Yu_Vn_Dm_au_Lyrics_V_(getmp3.pro).mp3",
+"https://manhhung1606.github.io/manhhung/y2mate.com - Alan Walker Sabrina Carpenter Farruko On My Way Official Alternate Music Video.mp3",
   ];
   //Random music
   let index = Math.floor(Math.random() * ManhHung_List.length);
