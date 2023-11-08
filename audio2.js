eddb0570a44713993192d03be84b69815cec0db9
manhhung1606 <88197704+manhhung1606@users.noreply.github.com>
2023-11-08 11:12:49 +0700

    Update audio2.js
    Signed-off-by: manhhung1606 <88197704+manhhung1606@users.noreply.github.com>

--- audio2.js
+++ audio2.js
@@ -3,14 +3,11 @@ ManhHungAudio();
 }
 const ManhHung_List = [
 "https://manhhung1606.github.io/manhhung/Chờ đợi quá khứ.mp3",
-"https://manhhung1606.github.io/manhhung/mv_anh_tin_minh_da_cho_nhau_mot_ky_niem_thu_thuy_ft_luong_bang_quang_6771119687899202332.mp3",
 "https://manhhung1606.github.io/manhhung/vietsub_ai_phi_hau_huyen_-224110807638924782.mp3",
 "https://manhhung1606.github.io/manhhung/y2meta.com - Ngồi Bên Em (320 kbps).mp3",
 "https://manhhung1606.github.io/manhhung/Mt_Bc_Yu_Vn_Dm_au_Lyrics_V_(getmp3.pro).mp3",
 "https://manhhung1606.github.io/manhhung/X2Download.app - █▬█ █ ▀█▀ Bài Học Trong Cuộc Sống - Tiay (Rap Việt 2012) (320 kbps).mp3",
 "https://manhhung1606.github.io/manhhung/AI_L_NGI_THNG_EM_-_QUN_AP_L_(getmp3.pro).mp3",
-"https://manhhung1606.github.io/manhhung/Please Tell Me Why - Bảo Thy (ft. Vương Khang)  Lyrics Video.mp3",
-"https://manhhung1606.github.io/manhhung/Mong Anh Quay Về.mp3",
   ];
   //Random music
   let index = Math.floor(Math.random() * ManhHung_List.length);
