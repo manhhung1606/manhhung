a33ad5858dd7fec27c0f9613c439cd56ec5116b8
manhhung1606 <88197704+manhhung1606@users.noreply.github.com>
2026-04-25 22:28:04 +0700

    Update audio2.js
    Signed-off-by: manhhung1606 <88197704+manhhung1606@users.noreply.github.com>

--- audio2.js
+++ audio2.js
@@ -1,28 +1,39 @@
 function __MAIMAIMOTTINHYEU1606() {
-ManhHungAudio();
+  ManhHungAudio();
 }
+
 const ManhHung_List = [
-"https://manhhung1606.github.io/manhhung/Chờ đợi quá khứ.mp3",
-"https://manhhung1606.github.io/manhhung/vietsub_ai_phi_hau_huyen_-224110807638924782.mp3",
-"https://manhhung1606.github.io/manhhung/y2meta.com - Ngồi Bên Em (320 kbps).mp3",
-"https://manhhung1606.github.io/manhhung/Mt_Bc_Yu_Vn_Dm_au_Lyrics_V_(getmp3.pro).mp3",
-"https://manhhung1606.github.io/manhhung/X2Download.app - █▬█ █ ▀█▀ Bài Học Trong Cuộc Sống - Tiay (Rap Việt 2012) (320 kbps).mp3",
-"https://manhhung1606.github.io/manhhung/AI_L_NGI_THNG_EM_-_QUN_AP_L_(getmp3.pro).mp3",
-  ];
-  //Random music
-  let index = Math.floor(Math.random() * ManhHung_List.length);
-  const audio = new Audio(ManhHung_List[index]);
-  function ManhHungAudio() {audio.play();}
-  document.addEventListener("click", ManhHungAudio);
-  audio.addEventListener("ended", function() {
-    index = (index + 1) % ManhHung_List.length;
-    audio.src = ManhHung_List[index];
+  "https://manhhung1606.github.io/manhhung/Chờ đợi quá khứ.mp3",
+  "https://manhhung1606.github.io/manhhung/vietsub_ai_phi_hau_huyen_-224110807638924782.mp3",
+  "https://manhhung1606.github.io/manhhung/y2meta.com - Ngồi Bên Em (320 kbps).mp3",
+  "https://manhhung1606.github.io/manhhung/Mt_Bc_Yu_Vn_Dm_au_Lyrics_V_(getmp3.pro).mp3",
+  "https://manhhung1606.github.io/manhhung/X2Download.app - █▬█ █ ▀█▀ Bài Học Trong Cuộc Sống - Tiay (Rap Việt 2012) (320 kbps).mp3",
+  "https://manhhung1606.github.io/manhhung/AI_L_NGI_THNG_EM_-_QUN_AP_L_(getmp3.pro).mp3",
+];
+
+// Random nhạc
+let index = Math.floor(Math.random() * ManhHung_List.length);
+const audio = new Audio(ManhHung_List[index]);
+
+function ManhHungAudio() { audio.play(); }
+
+audio.addEventListener("ended", function () {
+  index = (index + 1) % ManhHung_List.length;
+  audio.src = ManhHung_List[index];
+  audio.play();
+});
+
+// Giọng đọc lời chào, xong rồi nhạc mới chạy
+window.addEventListener('load', function () {
+  const msg = new SpeechSynthesisUtterance('Chào mừng bạn đến với website của Mạnh Hùng');
+  msg.lang = 'vi-VN';
+  msg.rate = 0.9;
+
+  msg.onend = function () {
     audio.play();
-  });
-window.addEventListener('load', function() {
-    const msg = new SpeechSynthesisUtterance('Chào mừng bạn đến với website của Mạnh Hùng');
-    msg.lang = 'vi-VN';
-    document.addEventListener('click', function() {
-        window.speechSynthesis.speak(msg);
-    }, {once: true});
+  };
+
+  document.addEventListener('click', function () {
+    window.speechSynthesis.speak(msg);
+  }, { once: true });
 });
