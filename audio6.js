02142509fb6abd70e0607bf05eef4f307766a7e3
manhhung1606 <88197704+manhhung1606@users.noreply.github.com>
2026-04-27 21:26:30 +0700

    Update audio6.js
    Signed-off-by: manhhung1606 <88197704+manhhung1606@users.noreply.github.com>

--- audio6.js
+++ audio6.js
@@ -1,6 +1,6 @@
 // Danh sách nhạc
 const ManhHung_List = [
-  "https://manhhung1606.github.io/manhhung/Gặp Người Đúng Lúc [ ❣ Lời Việt ❣ ] [SASAH-gigmQ].mp3",
+  "https://manhhung1606.github.io/manhhung/B-Complex - Beautiful Lies [tzqw_Dqa0SU].mp3",
   "https://manhhung1606.github.io/manhhung/Nếu Ánh Trăng Không Đến Remix ( Dj QT FunkyHouse ) [RcmyYv_ZF8M].mp3",
   "https://manhhung1606.github.io/manhhung/Alan Walker - On My Way Remix ( Dj QT Mix ) [iG9FkRGu6Po].mp3",
   "https://manhhung1606.github.io/manhhung/Alan Walker - Play Remix ( Dj QT Mix ).mp3",
