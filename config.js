8cc04224f69ffe46f70c3c5aa74547528428ec7c
manhhung1606 <88197704+manhhung1606@users.noreply.github.com>
2026-04-29 12:18:48 +0700

    Update config.js
    Signed-off-by: manhhung1606 <88197704+manhhung1606@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Hi, chào cậu ! Mình là  Mạnh Hùng...',
-    introDesc: ` (∪ ◡ ∪) ...NMH... (*^ -^*) `,
+    introDesc: ` (∪ ◡ ∪) ...Vào nghe nhạc nha bạn iu... (*^ -^*) `,
     btnIntro: ' Ok, lest go! ',
     title: ' Đằng ấy có thấy đẹp không? ',
     desc: ' Nếu mà nói dối là đằng ấy sẽ ế suốt đời😂... ',
@@ -9,8 +9,8 @@ const CONFIG = {
     question: ' Thật hả ? ',
     btnReply: ' Send to Xấu zai từ bé ',
     reply: ' Thật mà...hjhj... Tớ mà nói điêu thì tớ làm chó😂... ',
-    mess: ' 👍 ',
-    messDesc: ' Yêu thế nhờ...! ',
-    btnAccept: ' Mình biết mà .hiihaa ',
+    mess: ' 💓💓💓 ',
+    messDesc: ' Mình biết mà .hiihaa...!',
+    btnAccept: ' 🫰🫰🫰 ',
     messLink: '',
 }
